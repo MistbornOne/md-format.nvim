17b7314cb06c59341aae7c440f4410afe4b8767d
MistbornOne <iankwatkins@gmail.com>
2025-05-21 11:28:53 -0400

    add normal mode

--- lua/md-format/commands.lua
+++ lua/md-format/commands.lua
@@ -1,6 +1,7 @@
 local M = {}
 local format = require("md-format.format")
 
+-- Visual Mode Commands
 function M.setup()
 	vim.api.nvim_create_user_command("MDFormatBold", function()
 		format.format_visual("bold")
@@ -29,6 +30,11 @@ function M.setup()
 	vim.api.nvim_create_user_command("MDFormatHl", function()
 		format.format_visual("highlight")
 	end, { range = true })
+
+	-- Normal Mode Commands
+	vim.api.nvim_create_user_command("MDFormatBoldWord", function()
+		format.format_current_word("bold")
+	end, {})
 end
 
 return M
--- lua/md-format/format.lua
+++ lua/md-format/format.lua
@@ -2,6 +2,7 @@ local core = require("md-format.core")
 
 local M = {}
 
+-- Format in visual mode
 function M.format_visual(format_type)
 	local start_pos = vim.fn.getpos("'<")
 	local end_pos = vim.fn.getpos("'>")
@@ -13,6 +14,24 @@ function M.format_visual(format_type)
 	local formatted_lines = vim.split(formatted, "\n", { plain = true })
 
 	vim.api.nvim_buf_set_lines(0, start_pos[2] - 1, end_pos[2], false, formatted_lines)
+end
+
+-- Format in normal mode
+function M.format_current_word(format_type)
+	local word = vim.fn.expand("<cword>")
+	if not word or word == "" then
+		return
+	end
+
+	local formatted = core.apply(format_type, word)
+
+	-- Replace the word under the cursor with formatted version
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line = vim.api.nvim_get_current_line()
+	local pattern = "\\V\\<" .. vim.fn.escape(word, "\\") .. "\\>"
+	local updated = line:gsub(pattern, formatted, 1)
+
+	vim.api.nvim_set_current_line(updated)
 end
 
 return M
